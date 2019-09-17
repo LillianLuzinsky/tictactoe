377ceb33afde8f52c9061a22c8099f0b18a080dc
Lillian Luzinsky <eyeris@tpg.com.au>
2019-09-17 21:20:48 +1000

    delete main.js not needed

--- main.js
+++
@@ -1,8 +0,0 @@
-if ((tiles[0] === p1Icon && tiles[1] === p1Icon && tiles[2] === p1Icon && currentPlayer !== -1) || 
-        (tiles[3] === p1Icon && tiles[4] === p1Icon && tiles[5] === p1Icon && currentPlayer !== -1) ||
-        (tiles[6] === p1Icon && tiles[7] === p1Icon && tiles[8] === p1Icon && currentPlayer !== -1) ||
-        (tiles[0] === p1Icon && tiles[3] === p1Icon && tiles[6] === p1Icon && currentPlayer !== -1) ||
-        (tiles[1] === p1Icon && tiles[4] === p1Icon && tiles[7] === p1Icon && currentPlayer !== -1) ||
-        (tiles[2] === p1Icon && tiles[5] === p1Icon && tiles[8] === p1Icon && currentPlayer !== -1) ||
-        (tiles[0] === p1Icon && tiles[4] === p1Icon && tiles[8] === p1Icon && currentPlayer !== -1) ||
-        (tiles[2] === p1Icon && tiles[4] === p1Icon && tiles[6] === p1Icon && currentPlayer !== -1) ){
